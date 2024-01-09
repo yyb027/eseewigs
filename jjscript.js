5aa4065f02f0a573dcb30394c768711dc2810f97
yyb027 <yyb027@hotmail.com>
2024-01-09 14:22:50 +0800

    Add files via upload

--- jjscript.js
+++ jjscript.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // 获取父容器
-    var container = document.querySelector('.parent-container');
+    // Get the parent container of the elements
+    var container = document.querySelector('.default_products_list');
 
-    // 获取要交换的两个元素
-    var listCategoryDescription = document.querySelector('.list_category_description editor_txt ck-content');
-    var themesPlistRight = document.querySelector('.themes_plist_right');
+    // Get the two elements you want to swap
+    var firstElement = container.querySelector('.list_category_description');
+    var secondElement = container.querySelector('.themes_plist_right'); // or '.list_category'
 
-    // 把`.themes_plist_right`移动到`.list_category_description`之前
-    container.insertBefore(themesPlistRight, listCategoryDescription);
-
-    // 或者，如果你想交换它们的位置，可以这样做
-    // container.insertBefore(listCategoryDescription, themesPlistRight.nextSibling);
+    // Swap the elements
+    container.insertBefore(secondElement, firstElement);
 });
